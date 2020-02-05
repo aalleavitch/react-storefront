1d831286929cbd42cdefc931d33fe4fca7df01ca
aalleavitch <alexander.alleavitch@thetiebar.com>
2020-02-05 13:10:28 -0600

    Added test for menuButtonRenderer

--- packages/react-storefront/test/NavTabs.test.js
+++ packages/react-storefront/test/NavTabs.test.js
@@ -163,4 +163,32 @@ describe('NavTabs', () => {
       )
     ).toMatchSnapshot()
   })
+
+  
+  it('should add a menu button to the tab defined by menuButtonRenderer', () => {
+    const renderMenuButton = (menu) => {
+      return (
+        <button
+          id={'#menu-button'}
+          onClick={() => menu}
+        >
+        </button>
+      )
+    }
+
+    const component = (
+      <Provider app={app}>
+        <NavTab menuButtonRenderer={renderMenuButton}>
+          {'menu'}
+        </NavTab>
+      </Provider>
+    )
+
+    expect(
+      mount(component)
+        .find('#menu-button')
+        .at(0)
+        .simulate('click')
+    ).toHaveReturnedWith('menu')
+  })
 })
